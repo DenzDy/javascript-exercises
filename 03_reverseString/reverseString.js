6316d50f6278f357ad3800dff5c30de8a49f3ed9
Denzell Robyn Dy <dqdy@up.edu.ph>
2023-02-09 15:54:06 +0800

    Complete tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var arrayOfString = string.split("");
+    arrayOfString.reverse();
+    return arrayOfString.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numberArgs) {
+    var finalArray = array;
+    for(let arg of numberArgs){
+        console.log(arg);
+        for(let i = 0; i < finalArray.length; i++){
+            if(arg === array[i]){
+                finalArray.splice(i, 1);
+            }
+        }
+    }
+    return finalArray;
 };
 
 // Do not edit below this line
